44b4045b94def6971ae900d7ef9d2dd640847855
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-05-22 02:31:01 +0600

    Done

---
+++ index.js
@@ -0,0 +1,90 @@
+//It's from (expressjs) by searching in google
+const express = require('express')
+const bodyParser = require('body-parser');
+const port = 3001
+const cors = require('cors');
+// const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const got = require('got');
+// const rp = require('request-promise');
+//It's from (dotenv npm) by searching in google
+require('dotenv').config();
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_USER = process.env.DB_USER;
+// console.log(DB_NAME);
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
+// const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dmyuc.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ykx4x.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+//It's from (expressjs) by searching in google
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// console.log("Link from website");
+// const url = "https://www.w3schools.com/";
+async function loadData(url){
+    // console.log(url)
+    try{
+        const response = await got(url);
+        const html = response.body;
+        // console.log(html);
+        const $ = cheerio.load(html);
+        const linkObjects = $('a');
+        const links = [];
+        linkObjects.each((idx, element) =>{
+            links.push({
+                text: $(element).text(),
+                href: $(element).attr('href')
+            });
+        });
+        // console.log(links);
+        // sendingServer(links);
+        return links;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+app.get('/', (req, res) =>{
+    res.send("Hello Heroku");
+});
+
+app.get('/all-links', (req, res) => {
+    const url = req.query.url;
+    // console.log(url);
+    const data = loadData(url);
+    // console.log(data);
+    data.then( links => {
+        // console.log({links});
+        res.send(links);
+    })
+})
+// loadData(url);
+// console.log(data);
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log("Mongo Connected")
+    const linksCollection = client.db(DB_NAME).collection("all_links");
+    // perform actions on the collection object
+    app.post('/', (req, res) => {
+        const allLinks = req.body;
+        linksCollection.deleteMany({})
+        .then( data => {
+            linksCollection.insertMany(allLinks)
+            .then( links => {
+                // console.log(links.ops);
+                res.send(links.ops);
+            })
+        })
+        
+    })
+
+});
+
+app.listen(process.env.PORT || port);
